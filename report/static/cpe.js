50a6a84a288c55c98c5892815cc30a6fe24dfb50
yuii88 <pim8828@gmail.com>
2021-10-11 16:11:56 +0700

    add 2 files

---
+++ report/static/cpe.js
@@ -0,0 +1,18 @@
+
+$(document).ready( function () {
+    setNavigation();
+    $('#maintable').DataTable();
+} );
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+
+    $(".nav a").each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(1, href.length+1) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}
