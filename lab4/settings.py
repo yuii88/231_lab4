2b7f86642cc77f5e70b87c8611abbfb5fe0954ac
yuii88 <pim8828@gmail.com>
2021-10-25 14:50:37 +0700

    setting database pgAdmin4

--- lab4/settings.py
+++ lab4/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'lab4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CPE231',
+        'USER': 'postgres',
+        'PASSWORD': 'CPE231',
+        'HOST': 'localhost',
+        'PORT': '',
+        }
 }
 
 
